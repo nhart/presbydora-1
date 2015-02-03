57f3e99a1c879e8795dbcebb36124ac25adecee0
MorganDawe <mdawe@discoverygarden.ca>
2015-02-03 14:45:12 -0400

    Initial Commit

---
+++ theme/site-name.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+
+<div class="footer-site-name">
+  <?php if (variable_get('site_name', "")): ?>
+  <h1 id="site-name">
+    <span><?php print variable_get('site_name', ""); ?></span>
+  </h1>
+  <?php endif; ?>
+</div>
